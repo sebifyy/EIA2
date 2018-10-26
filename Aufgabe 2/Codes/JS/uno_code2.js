2c0173f037fd599ca722b2bcb3b3cfcc3299710b
sebas <sebas@LAPTOP-JN309QDE.local>
2018-10-26 16:57:37 +0200

    Uno neuer Code

---
+++ Aufgabe 2/Codes/JS/uno_code2.js
@@ -0,0 +1,34 @@
+var unogame2;
+(function (unogame2) {
+    let colors = ["Red", "Blue", "Yellow", "Green"];
+    let values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "RW"];
+    var handCards = [];
+    var deck = [];
+    for (let i = 0; i < colors.length; i++) {
+        for (let a = 0; a < values.length; a++) {
+            for (let z = 0; z < 2; z++) {
+                var card = { color: colors[i], value: values[i] };
+            }
+            deck.push(card);
+        }
+    }
+    var userInput = prompt("Wie viele Karten sollen ausgeteilt werden?");
+    var counter = Number(userInput);
+    function randomCards(amount = counter) {
+        for (let i = amount; i > 0; i--) {
+            let rCard = Math.random() * (deck.length - 1);
+            handCards.push(deck[rCard]);
+            deck.splice(rCard, 1);
+        }
+    }
+    function displayCards() {
+        for (let i = 0; i < handCards.length; i++) {
+            let x = document.getElementById("HandCards");
+            x.innerHTML = handCards[i].value;
+        }
+    }
+    randomCards();
+    displayCards();
+    console.log(card);
+})(unogame2 || (unogame2 = {}));
+//# sourceMappingURL=uno_code2.js.map
