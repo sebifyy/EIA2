e656a59dc40789a1d73714c62c960e8af5c8102a
sebas <sebas@LAPTOP-JN309QDE>
2018-11-11 15:11:56 +0100

    Aufgabe 3, drawCard(space)

--- Aufgabe 3/Codes/JS/uno_code2.js
+++ Aufgabe 3/Codes/JS/uno_code2.js
@@ -1,60 +1,83 @@
-var unogame2;
-(function (unogame2) {
+var unogame3;
+(function (unogame3) {
     //KARTEN DIE NICHT PER FUNKTION GENERIERT WERDEN
     //FARBWUNSCH
     let w1 = {
         color: "black",
-        value: "Farbwunsch",
+        value: "Farbwunsch"
     };
     let w2 = {
         color: "black",
-        value: "Farbwunsch",
+        value: "Farbwunsch"
     };
     let w3 = {
         color: "black",
-        value: "Farbwunsch",
+        value: "Farbwunsch"
     };
     let w4 = {
         color: "black",
-        value: "Farbwunsch",
+        value: "Farbwunsch"
     };
     //+4 JOKER
     let j1 = {
         color: "black",
-        value: "+4",
+        value: "+4"
     };
     let j2 = {
         color: "black",
-        value: "+4",
+        value: "+4"
     };
     let j3 = {
         color: "black",
-        value: "+4",
+        value: "+4"
     };
     let j4 = {
         color: "black",
-        value: "+4",
+        value: "+4"
     };
+    //NULL
     let zeroR = {
         color: "Red",
-        value: "0",
+        value: "0"
     };
     let zeroB = {
         color: "Blue",
-        value: "0",
+        value: "0"
     };
     let zeroY = {
         color: "Yellow",
-        value: "0",
+        value: "0"
     };
     let zeroG = {
         color: "Green",
-        value: "0",
+        value: "0"
     };
     let colors = ["Red", "Blue", "Yellow", "Green"];
     let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "RW"];
     var handCards = [];
     var deck = [w1, w2, w3, w4, j1, j2, j3, j4, zeroR, zeroB, zeroY, zeroG];
+    function listeners() {
+        document.getElementById("draw").addEventListener("click", getNewCard);
+        document.addEventListener("keyup", getNewCardSpace);
+        //for (let i: number = 0; i < handCards.length; i++) {
+        //document.getElementById(String(handCards.indexOf(i)));
+        //   }
+        // document.getElementById("sort").addEventListener("click", sortByColor);
+    }
+    function getNewCard() {
+        randomCards(1);
+        displayCards();
+        //console.log(_event.key == " ");
+    }
+    function getNewCardSpace(_event) {
+        if (_event.key == " ") {
+            randomCards(1);
+            displayCards();
+        }
+    }
+    //function sortByColor(): void {
+    // }
+    // ________________________________________________GENERIERUNG DER MEISTEN KARTEN__________________________________________
     for (let i = 0; i < colors.length; i++) {
         for (let a = 0; a < values.length; a++) {
             for (let z = 0; z < 2; z++) {
@@ -63,6 +86,7 @@ var unogame2;
             }
         }
     }
+    //________________________________________________USER EINGABE___________________________________________________
     var userInput = prompt("Wie viele Karten sollen ausgeteilt werden?");
     var counter = Number(userInput);
     function randomCards(amount = counter) {
@@ -72,17 +96,21 @@ var unogame2;
             deck.splice(rCard, 1);
         }
     }
+    //_____________________________________________KARTEN ANZEIGEN____________________________________________________________
     function displayCards() {
+        document.getElementById("HandCards").innerHTML = "";
         for (let i = 0; i < handCards.length; i++) {
             let x = document.createElement("div");
             x.innerText = handCards[i].value;
             x.classList.add("card");
             x.classList.add(handCards[i].color);
+            x.setAttribute("id", String([i]));
             document.getElementById("HandCards").appendChild(x);
         }
     }
     randomCards();
     displayCards();
+    listeners();
     console.log(card);
-})(unogame2 || (unogame2 = {}));
+})(unogame3 || (unogame3 = {}));
 //# sourceMappingURL=uno_code2.js.map
--- Aufgabe 3/Codes/JS/uno_code2.ts
+++ Aufgabe 3/Codes/JS/uno_code2.ts
@@ -1,4 +1,4 @@
-namespace unogame2 {
+namespace unogame3 {
     interface Card {
         color: string;
         value: string;
@@ -8,64 +8,65 @@ namespace unogame2 {
     //FARBWUNSCH
     let w1: Card = {
         color: "black",
-        value: "Farbwunsch",
-    }
+        value: "Farbwunsch"
+    };
 
     let w2: Card = {
         color: "black",
-        value: "Farbwunsch",
-    }
+        value: "Farbwunsch"
+    };
 
     let w3: Card = {
         color: "black",
-        value: "Farbwunsch",
-    }
+        value: "Farbwunsch"
+    };
 
     let w4: Card = {
         color: "black",
-        value: "Farbwunsch",
-    }
+        value: "Farbwunsch"
+    };
 
     //+4 JOKER
     let j1: Card = {
         color: "black",
-        value: "+4",
-    }
+        value: "+4"
+    };
 
     let j2: Card = {
         color: "black",
-        value: "+4",
-    }
+        value: "+4"
+    };
 
     let j3: Card = {
         color: "black",
-        value: "+4",
-    }
+        value: "+4"
+    };
 
     let j4: Card = {
         color: "black",
-        value: "+4",
-    }
+        value: "+4"
+    };
 
+    //NULL
     let zeroR: Card = {
         color: "Red",
-        value: "0",
-    }
+        value: "0"
+    };
 
     let zeroB: Card = {
         color: "Blue",
-        value: "0",
-    }
+        value: "0"
+    };
 
     let zeroY: Card = {
         color: "Yellow",
-        value: "0",
-    }
+        value: "0"
+    };
 
     let zeroG: Card = {
         color: "Green",
-        value: "0",
-    }
+        value: "0"
+    };
 
     let colors: string[] = ["Red", "Blue", "Yellow", "Green"];
     let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "RW"];
@@ -73,6 +74,33 @@ namespace unogame2 {
     var deck: Card[] = [w1, w2, w3, w4, j1, j2, j3, j4, zeroR, zeroB, zeroY, zeroG];
 
 
+    function listeners(): void {
+        document.getElementById("draw").addEventListener("click", getNewCard);
+        document.addEventListener("keyup", getNewCardSpace);
+        //for (let i: number = 0; i < handCards.length; i++) {
+        //document.getElementById(String(handCards.indexOf(i)));
+        //   }
+        // document.getElementById("sort").addEventListener("click", sortByColor);
+    }
+
+
+    function getNewCard(): void {
+        randomCards(1);
+        displayCards();
+        //console.log(_event.key == " ");
+    }
+
+    function getNewCardSpace(_event: KeyboardEvent): void {
+        if (_event.key == " ") {
+            randomCards(1);
+            displayCards();
+        }
+    }
+    //function sortByColor(): void {
+    // }
+
+
+    // ________________________________________________GENERIERUNG DER MEISTEN KARTEN__________________________________________
     for (let i: number = 0; i < colors.length; i++) {
         for (let a: number = 0; a < values.length; a++) {
             for (let z: number = 0; z < 2; z++) {
@@ -82,7 +110,7 @@ namespace unogame2 {
         }
     }
 
-
+    //________________________________________________USER EINGABE___________________________________________________
     var userInput: string = prompt("Wie viele Karten sollen ausgeteilt werden?");
     var counter: number = Number(userInput);
 
@@ -94,17 +122,22 @@ namespace unogame2 {
         }
     }
 
+
+    //_____________________________________________KARTEN ANZEIGEN____________________________________________________________
     function displayCards(): void {
+        document.getElementById("HandCards").innerHTML = "";
         for (let i: number = 0; i < handCards.length; i++) {
             let x: HTMLElement = document.createElement("div");
             x.innerText = handCards[i].value;
             x.classList.add("card");
             x.classList.add(handCards[i].color);
+            x.setAttribute("id", String([i]));
             document.getElementById("HandCards").appendChild(x);
         }
     }
 
     randomCards();
     displayCards();
+    listeners();
     console.log(card);
 }
