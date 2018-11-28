803c853c56c79dc8b5c5e25acc273ceb4a8f1fdc
sebas <sebas@LAPTOP-JN309QDE.fritz.box>
2018-11-28 18:53:26 +0100

    aufg6 server.ts

---
+++ Aufgabe 6/Server/Server.ts
@@ -0,0 +1,28 @@
+import * as Http from "http";                       //Kreiert http Objekt und hängt es an 'http' Tag an
+
+namespace L06_SendData {
+    console.log("Starting server");
+    let port: number = process.env.PORT;            //Legt fest, auf welchen Port der Server "hoeren" soll.
+    if (port == undefined)                          //Falls dieser undefiniert ist,
+        port = 8100;                                //definieren wir selber den Port "8100"
+
+    let server: Http.Server = Http.createServer();  //Kreiert einen Server auf dem lokalen Rechner 
+    server.addListener("request", handleRequest);   //Eventlistener, der bei einer "request" (Anfrage) handleRequest() ausführt
+    server.addListener("listening", handleListen);  //handleListen() wird aufgerufen, wenn man auf den Server zugegriffen hat
+    server.listen(port);                            //Es wird geschaut, ob auf den Port zugegriffen wird
+
+    function handleListen(): void {                 //Neue function
+        console.log("Listening");                   //"Listening" wird in der console ausgegeben
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {      //Neue function
+        console.log("I hear voices!");                                                                  //Konsolenausgabe "I hear voices"
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");                                //html Dokument wird im <header> verändert
+        _response.setHeader("Access-Control-Allow-Origin", "*");                                        //html Dokument wird im <header> verändert
+
+        _response.write(_request.url);                                                                  //Sendet <header> Informationen an Client
+
+        _response.end();                                                                                //Beendet das Senden der Serverantwort
+    }
+}
